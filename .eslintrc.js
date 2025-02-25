33bc5e61d2bfb0e2aabb21e0431b3410488ceabc
王卫国 <wbwangwg@szlanyou.com>
2025-02-25 10:58:15 +0800

    add coding rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,212 +3,329 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    es6: true,
+    es6: true
   },
-  extends: [
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-    "plugin:vue/vue3-essential",
-  ],
-  plugins: ["prettier"],
+  extends: ['eslint:recommended', '@vue/typescript/recommended', 'plugin:prettier/recommended', 'plugin:vue/vue3-essential'],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module'
   },
   // add your custom rules here
-  //it is base on https://github.com/vuejs/eslint-config-vue
+  // it is base on https://github.com/vuejs/eslint-config-vue
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/max-attributes-per-line": [2, {
-      "singleline": 10,
-      "multiline": {
-        "max": 1,
-        "allowFirstLine": false
+    // 不允许出现console语句
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 不允许出现debugger语句
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // enforce the maximum number of attributes per line
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 10
+        },
+        multiline: {
+          max: 1
+        }
       }
-    }],
-    "vue/singleline-html-element-content-newline": "off",
-    "vue/multiline-html-element-content-newline":"off",
-    "vue/name-property-casing": ["error", "PascalCase"],
-    "vue/no-v-html": "off",
-    'accessor-pairs': 2,
-    'arrow-spacing': [2, {
-      'before': true,
-      'after': true
-    }],
-    'block-spacing': [2, 'always'],
-    'brace-style': [2, '1tbs', {
-      'allowSingleLine': true
-    }],
-    'camelcase': [0, {
-      'properties': 'always'
-    }],
-    'comma-dangle': [2, 'never'],
-    'comma-spacing': [2, {
-      'before': false,
-      'after': true
-    }],
-    'comma-style': [2, 'last'],
-    'constructor-super': 2,
-    'curly': [2, 'multi-line'],
-    'dot-location': [2, 'property'],
-    'eol-last': 2,
-    'eqeqeq': ["off"],
-    'generator-star-spacing': [2, {
-      'before': true,
-      'after': true
-    }],
-    'handle-callback-err': [2, '^(err|error)$'],
-    'indent': [2, 2, {
-      'SwitchCase': 1
-    }],
-    'jsx-quotes': [2, 'prefer-single'],
-    'key-spacing': [2, {
-      'beforeColon': false,
-      'afterColon': true
-    }],
-    'keyword-spacing': [2, {
-      'before': true,
-      'after': true
-    }],
-    'new-cap': [2, {
-      'newIsCap': true,
-      'capIsNew': false
-    }],
-    'new-parens': 2,
-    'no-array-constructor': 2,
-    'no-caller': 2,
-    'no-class-assign': 2,
-    'no-cond-assign': 2,
-    'no-const-assign': 2,
-    'no-control-regex': 0,
-    'no-delete-var': 2,
-    'no-dupe-args': 2,
-    'no-dupe-class-members': 2,
-    'no-dupe-keys': 2,
-    'no-duplicate-case': 2,
-    'no-empty-character-class': 2,
-    'no-empty-pattern': 2,
-    'no-eval': 2,
-    'no-ex-assign': 2,
-    'no-extend-native': 2,
-    'no-extra-bind': 2,
-    'no-extra-boolean-cast': 2,
-    'no-extra-parens': [2, 'functions'],
-    'no-fallthrough': 2,
-    'no-floating-decimal': 2,
-    'no-func-assign': 2,
-    'no-implied-eval': 2,
-    'no-inner-declarations': [2, 'functions'],
-    'no-invalid-regexp': 2,
-    'no-irregular-whitespace': 2,
-    'no-iterator': 2,
-    'no-label-var': 2,
-    'no-labels': [2, {
-      'allowLoop': false,
-      'allowSwitch': false
-    }],
-    'no-lone-blocks': 2,
-    'no-mixed-spaces-and-tabs': 2,
-    'no-multi-spaces': 2,
-    'no-multi-str': 2,
-    'no-multiple-empty-lines': [2, {
-      'max': 1
-    }],
-    'no-native-reassign': 2,
-    'no-negated-in-lhs': 2,
-    'no-new-object': 2,
-    'no-new-require': 2,
-    'no-new-symbol': 2,
-    'no-new-wrappers': 2,
-    'no-obj-calls': 2,
-    'no-octal': 2,
-    'no-octal-escape': 2,
-    'no-path-concat': 2,
-    'no-proto': 2,
-    'no-redeclare': 2,
-    'no-regex-spaces': 2,
-    'no-return-assign': [2, 'except-parens'],
-    'no-self-assign': 2,
-    'no-self-compare': 2,
-    'no-sequences': 2,
-    'no-shadow-restricted-names': 2,
-    'no-spaced-func': 2,
-    'no-sparse-arrays': 2,
-    'no-this-before-super': 2,
-    'no-throw-literal': 2,
-    'no-trailing-spaces': 2,
-    'no-undef': 2,
-    'no-undef-init': 2,
-    'no-unexpected-multiline': 2,
-    'no-unmodified-loop-condition': 2,
-    'no-unneeded-ternary': [2, {
-      'defaultAssignment': false
-    }],
-    'no-unreachable': 2,
-    'no-unsafe-finally': 2,
-    'no-unused-vars': [2, {
-      'vars': 'all',
-      'args': 'none'
-    }],
-    'no-useless-call': 2,
-    'no-useless-computed-key': 2,
-    'no-useless-constructor': 2,
-    'no-useless-escape': 0,
-    'no-whitespace-before-property': 2,
-    'no-with': 2,
-    'one-var': [2, {
-      'initialized': 'never'
-    }],
-    'operator-linebreak': [2, 'after', {
-      'overrides': {
-        '?': 'before',
-        ':': 'before'
+    ],
+    // require a line break before and after the contents of a singleline element
+    'vue/singleline-html-element-content-newline': 'off',
+    // require a line break before and after the contents of a multiline element
+    'vue/multiline-html-element-content-newline': 'off',
+    // enforce specific casing for the name property in Vue components
+    'vue/name-property-casing': ['error', 'PascalCase'],
+    // disallow use of v-html to prevent XSS attack
+    'vue/no-v-html': 'off',
+    // disallow parsing errors in <template>
+    'vue/no-parsing-error': 'error',
+    // enforce order of properties in components
+    'vue/order-in-components': 'error',
+    // require or disallow a line break before tag's closing brackets
+    'vue/html-closing-bracket-newline': 'error',
+    // disallow using v-if on the same element as v-for
+    'vue/no-use-v-if-with-v-for': [
+      'error',
+      {
+        allowUsingIterationVar: false
       }
-    }],
-    'padded-blocks': [2, 'never'],
-    'quotes': [2, 'single', {
-      'avoidEscape': true,
-      'allowTemplateLiterals': true
-    }],
-    'semi': [2, 'never'],
-    'semi-spacing': [2, {
-      'before': false,
-      'after': true
-    }],
-    'space-before-blocks': [2, 'always'],
-    'space-before-function-paren': [2, 'never'],
-    'space-in-parens': [2, 'never'],
-    'space-infix-ops': 2,
-    'space-unary-ops': [2, {
-      'words': true,
-      'nonwords': false
-    }],
-    'spaced-comment': [2, 'always', {
-      'markers': ['global', 'globals', 'eslint', 'eslint-disable', '*package', '!', ',']
-    }],
-    'template-curly-spacing': [2, 'never'],
-    'use-isnan': 2,
-    'valid-typeof': 2,
-    'wrap-iife': [2, 'any'],
-    'yield-star-spacing': [2, 'both'],
-    'yoda': [2, 'never'],
-    'prefer-const': 2,
-    'object-curly-spacing': [2, 'always', {
-      objectsInObjects: false
-    }],
-    'array-bracket-spacing': [2, 'never']
+    ],
+    // disallow duplication of field names
+    'vue/no-dupe-keys': 'error',
+    // 在对象中使用getter/setter
+    'accessor-pairs': 'error',
+    // Enforce consistent spacing inside array brackets
+    'array-bracket-spacing': ['error', 'never'],
+    // Enforce consistent spacing before and after the arrow in arrow functions, => 的前/后括号
+    'arrow-spacing': [
+      'error',
+      {
+        before: true,
+        after: true
+      }
+    ],
+    // Disallow or enforce spaces inside of blocks after opening block and before closing block
+    'block-spacing': ['error', 'always'],
+    // Enforce consistent brace style for blocks
+    'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    // Enforce camelcase naming convention
+    camelcase: ['error', { properties: 'always' }],
+    // Require or disallow trailing commas,是否允许对象中出现结尾逗号
+    'comma-dangle': ['error', 'never'],
+    // Enforce consistent spacing before and after commas
+    'comma-spacing': ['error', { before: false, after: true }],
+    // Enforce consistent comma style
+    'comma-style': ['error', 'last'],
+    // Enforce consistent spacing inside computed property brackets
+    'computed-property-spacing': ['error', 'never'],
+    // Require super() calls in constructors
+    'constructor-super': 'error',
+    // Enforce consistent brace style for all control statements
+    curly: ['error', 'multi-line'],
+    // Enforce consistent newlines before and after dots
+    'dot-location': ['error', 'property'],
+    // Require or disallow newline at the end of files
+    'eol-last': ['error', 'always'],
+    // Require the use of === and !==
+    eqeqeq: ['error', 'always'],
+    // Enforce consistent spacing around * operators in generator functions
+    'generator-star-spacing': ['error', { before: true, after: true }],
+    // Require error handling in callbacks
+    'handle-callback-err': ['error', '^(err|error)$'],
+    // Enforce consistent indentation
+    indent: ['error', 2, { SwitchCase: 1 }],
+    // Enforce the consistent use of either double or single quotes in JSX attributes
+    'jsx-quotes': ['error', 'prefer-single'],
+    // Enforce consistent spacing between keys and values in object literal properties
+    'key-spacing': [
+      'error',
+      {
+        beforeColon: false,
+        afterColon: true
+      }
+    ],
+    // Enforce consistent spacing before and after keywords
+    'keyword-spacing': ['error', { before: true, after: true }],
+    // Require constructor names to begin with a capital letter
+    'new-cap': ['error', { newIsCap: true, capIsNew: false }],
+    // Enforce or disallow parentheses when invoking a constructor with no arguments
+    'new-parens': 'error',
+    // Disallow Array constructors
+    'no-array-constructor': 'error',
+    // Disallow the use of arguments.caller or arguments.callee
+    'no-caller': 'error',
+    // Disallow reassigning class members
+    'no-class-assign': 'error',
+    // Disallow assignment operators in conditional expressions
+    'no-cond-assign': 'error',
+    // Disallow reassigning const variables
+    'no-const-assign': 'error',
+    // Disallow control characters in regular expressions
+    'no-control-regex': 'off',
+    // Disallow deleting variables
+    'no-delete-var': 'error',
+    // Disallow duplicate arguments in function definitions
+    'no-dupe-args': 'error',
+    // Disallow duplicate class members
+    'no-dupe-class-members': 'error',
+    // Disallow duplicate keys in object literals
+    'no-dupe-keys': 'error',
+    // Disallow duplicate case labels
+    'no-duplicate-case': 'error',
+    // Disallow empty character classes in regular expressions
+    'no-empty-character-class': 'error',
+    // Disallow empty destructuring patterns
+    'no-empty-pattern': 'error',
+    // Disallow the use of eval()
+    'no-eval': 'error',
+    // Disallow reassigning exceptions in catch clauses
+    'no-ex-assign': 'error',
+    // Disallow extending native types
+    'no-extend-native': 'error',
+    // Disallow unnecessary calls to .bind()
+    'no-extra-bind': 'error',
+    // Disallow unnecessary boolean casts
+    'no-extra-boolean-cast': 'error',
+    // Disallow unnecessary parentheses
+    'no-extra-parens': ['error', 'functions'],
+    // Disallow fallthrough of case statements
+    'no-fallthrough': 'error',
+    // Disallow leading or trailing decimal points in numeric literals
+    'no-floating-decimal': 'error',
+    // Disallow reassigning function declarations
+    'no-func-assign': 'error',
+    // Disallow the use of eval()-like methods
+    'no-implied-eval': 'error',
+    // Disallow variable or function declarations in nested blocks
+    'no-inner-declarations': ['error', 'functions'],
+    // Disallow invalid regular expression strings in RegExp constructors
+    'no-invalid-regexp': 'error',
+    // Disallow irregular whitespace
+    'no-irregular-whitespace': 'error',
+    // Disallow the use of the __iterator__ property
+    'no-iterator': 'error',
+    // Disallow labels that share a name with a variable
+    'no-label-var': 'error',
+    // Disallow labeled statements
+    'no-labels': ['error', { allowLoop: false, allowSwitch: false }],
+    // Disallow unnecessary nested blocks
+    'no-lone-blocks': 'error',
+    // Disallow mixed spaces and tabs for indentation
+    'no-mixed-spaces-and-tabs': 'error',
+    // Disallow multiple spaces
+    'no-multi-spaces': 'error',
+    // Disallow multiline strings
+    'no-multi-str': 'error',
+    // Disallow multiple empty lines
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    // Disallow assignments to native objects or read-only global variables
+    'no-native-reassign': 'error',
+    // Disallow negating the left operand in in expressions
+    'no-negated-in-lhs': 'error',
+    // Disallow Object constructors
+    'no-new-object': 'error',
+    // Disallow new operators with calls to require
+    'no-new-require': 'error',
+    // Disallow new operators with the Symbol object
+    'no-new-symbol': 'error',
+    // Disallow new operators with the String, Number, and Boolean objects
+    'no-new-wrappers': 'error',
+    // Disallow calling global object properties as functions
+    'no-obj-calls': 'error',
+    // Disallow octal literals
+    'no-octal': 'error',
+    // Disallow octal escape sequences in string literals
+    'no-octal-escape': 'error',
+    // Disallow string concatenation with __dirname and __filename
+    'no-path-concat': 'error',
+    // Disallow the use of the __proto__ property
+    'no-proto': 'error',
+    // Disallow variable redeclaration
+    'no-redeclare': 'error',
+    // Disallow multiple spaces in regular expressions
+    'no-regex-spaces': 'error',
+    // Disallow assignment operators in return statements
+    'no-return-assign': ['error', 'except-parens'],
+    // Disallow assignments where both sides are exactly the same
+    'no-self-assign': 'error',
+    // Disallow comparisons where both sides are exactly the same
+    'no-self-compare': 'error',
+    // Disallow comma operators
+    'no-sequences': 'error',
+    // Disallow identifiers from shadowing restricted names
+    'no-shadow-restricted-names': 'error',
+    // Disallow spacing between function identifiers and their applications (deprecated)
+    'no-spaced-func': 'error',
+    // Disallow sparse arrays
+    'no-sparse-arrays': 'error',
+    // Disallow this/super before calling super() in constructors
+    'no-this-before-super': 'error',
+    // Disallow throwing literals as exceptions
+    'no-throw-literal': 'error',
+    // Disallow trailing whitespace at the end of lines
+    'no-trailing-spaces': 'error',
+    // Disallow the use of undeclared variables unless mentioned in /*global */ comments
+    'no-undef': 'error',
+    // Disallow initializing variables to undefined
+    'no-undef-init': 'error',
+    // Disallow confusing multiline expressions
+    'no-unexpected-multiline': 'error',
+    // Disallow unmodified loop conditions
+    'no-unmodified-loop-condition': 'error',
+    // Disallow ternary operators when simpler alternatives exist
+    'no-unneeded-ternary': ['error', { defaultAssignment: false }],
+    // Disallow unreachable code after return, throw, continue, and break statements
+    'no-unreachable': 'error',
+    // Disallow control flow statements in finally blocks
+    'no-unsafe-finally': 'error',
+    // Disallow unused variables
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'none'
+      }
+    ],
+    // Disallow unnecessary calls to .call() and .apply()
+    'no-useless-call': 'error',
+    // Disallow unnecessary computed property keys in objects and classes
+    'no-useless-computed-key': 'error',
+    // Disallow unnecessary constructors
+    'no-useless-constructor': 'error',
+    // Disallow unnecessary escape characters
+    'no-useless-escape': 'off',
+    // Disallow whitespace before properties
+    'no-whitespace-before-property': 'error',
+    // Disallow with statements
+    'no-with': 'error',
+    // Enforce variables to be declared either together or separately in functions
+    'one-var': ['error', { initialized: 'never' }],
+    // Enforce consistent linebreak style for operators
+    'operator-linebreak': [
+      'error',
+      'after',
+      {
+        overrides: { '?': 'before', ':': 'before' }
+      }
+    ],
+    // Require or disallow padding within blocks
+    'padded-blocks': ['error', 'never'],
+    // Require using Error objects as Promise rejection reasons
+    'prefer-promise-reject-errors': ['error', { allowEmptyReject: true }],
+    // Enforce the consistent use of either backticks, double, or single quotes
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    // Require or disallow semicolons instead of ASI
+    semi: ['error', 'never'],
+    // Enforce consistent spacing before and after semicolons
+    'semi-spacing': ['error', { before: false, after: true }],
+    // Enforce consistent spacing before blocks
+    'space-before-blocks': ['error', 'always'],
+    // Enforce consistent spacing before function definition opening parenthesis
+    'space-before-function-paren': ['error', 'never'],
+    // Enforce consistent spacing inside parentheses
+    'space-in-parens': ['error', 'never'],
+    // Require spacing around infix operators
+    'space-infix-ops': 'error',
+    // Enforce consistent spacing before or after unary operators
+    'space-unary-ops': [
+      'error',
+      {
+        words: true,
+        nonwords: false
+      }
+    ],
+    // Enforce consistent spacing after the // or /* in a comment
+    'spaced-comment': [
+      'error',
+      'always',
+      {
+        markers: ['global', 'globals', 'eslint', 'eslint-disable', '*package', '!', ',']
+      }
+    ],
+    // Require or disallow spacing around embedded expressions of template strings
+    'template-curly-spacing': ['error', 'never'],
+    // Require calls to isNaN() when checking for NaN
+    'use-isnan': 'error',
+    // Enforce comparing typeof expressions against valid strings
+    'valid-typeof': 'error',
+    // Require parentheses around immediate function invocations
+    'wrap-iife': ['error', 'any'],
+    // Require or disallow spacing around the * in yield* expressions
+    'yield-star-spacing': ['error', 'both'],
+    // Require or disallow "Yoda" conditions
+    yoda: ['error', 'never'],
+    // Require const declarations for variables that are never reassigned after declared
+    'prefer-const': 'error',
+    // Enforce consistent spacing inside braces
+    'object-curly-spacing': ['error', 'always', { objectsInObjects: false }]
   },
   overrides: [
     {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
-        mocha: true,
-      },
-    },
-  ],
-};
+        mocha: true
+      }
+    }
+  ]
+}
--- src/views/demo/testInlieVw.vue
+++ src/views/demo/testInlieVw.vue
@@ -1,10 +1,8 @@
 <template>
   <div :style="{ width: pxToVw(100) }">Hello World</div>
-  <a href="https://www.cnblogs.com/scale/p/18539376"
-    >vant中组件的css单位没有转换</a
-  >
+  <a href="https://www.cnblogs.com/scale/p/18539376">vant中组件的css单位没有转换</a>
 </template>
 
 <script setup lang="ts">
-import { pxToVw } from "@/utils/pxToVw";
+import { pxToVw } from '@/utils/pxToVw'
 </script>
